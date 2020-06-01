56e739348c43b4f3f67337bbfca05c25dedbe16d
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-06-01 21:20:23 +0100

    UCorrect Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,45 @@
-import HtmlWebPackPlugin from "html-webpack-plugin";
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 // eslint-disable-next-line no-undef
-export const module = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
-    },
-    {
-      test: /\.html$/,
-      use: [
-        {
-          loader: "html-loader",
-        },
-      ],
-    },
-    {
-      test: /\.(png|svg|jpe?g|gif)$/,
-      include: /images/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            name: "[name].[ext]",
-            outputPath: "images/",
-            publicPath: "images/",
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
           },
-        },
-      ],
-    },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        include: /images/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+      favicon: "./public/statue.ico",
+    }),
   ],
 };
-export const plugins = [
-  new HtmlWebPackPlugin({
-    template: "./public/index.html",
-    filename: "./index.html",
-    favicon: "./public/statue.ico",
-  }),
-];
