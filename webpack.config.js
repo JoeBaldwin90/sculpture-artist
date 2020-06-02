b95055175c4e5a825844369cc3b36a2c68ad79d8
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-06-02 09:34:08 +0100

    Code splitting and improved webpack performance

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 // eslint-disable-next-line no-undef
 module.exports = {
+  mode: "production",
+  entry: {
+    index: path.resolve(__dirname, "src/index.js"),
+    hannah: path.resolve(__dirname, "src/images/hannah.png"),
+    head: path.resolve(__dirname, "src/images/head.png"),
+    clay: path.resolve(__dirname, "src/images/clay.jpg"),
+    arrow: path.resolve(__dirname, "src/images/arrow.svg"),
+  },
+  performance: {
+    hints: process.env.NODE_ENV === "production" ? "warning" : false,
+  },
   module: {
     rules: [
       {
@@ -40,6 +52,29 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
       favicon: "./public/statue.ico",
-    }),
+    })
   ],
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace("@", "")}`;
+          },
+        },
+      },
+    },
+  },
 };
