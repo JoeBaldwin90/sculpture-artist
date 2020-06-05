e67e44ee9f572a5e5ecf0608ee1718ceedc71359
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-06-05 18:54:36 +0100

    Homepage copy and responsive styles

--- src/components/homepage/HomepageStyles.js
+++ src/components/homepage/HomepageStyles.js
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 export const Image = styled.img.attrs((props) => ({
-  src: props.src,
-  alt: props.alt,
-}))`
+         src: props.src,
+         alt: props.alt,
+       }))`
   width: ${(props) => props.width};
   margin-bottom: 3em;
   opacity: ${(props) => (props.inView ? 1 : 0)};
   transition: opacity 1s ease;
+   @media (max-width: 425px) {
+    margin-bottom: 2em;
+   }
 `;
 
 export const MainGrid = styled.main`
@@ -24,11 +27,26 @@ export const MainGrid = styled.main`
 export const ContentWide = styled.div`
   grid-column: span 12;
   margin-bottom: 4em;
+
   div {
     display: flex;
     justify-content: space-between;
     p {
       width: calc(50% - 1em);
+    }
+  }
+
+  @media (max-width: 425px) {
+    margin-bottom: 2em;
+
+    div {
+      display: block;
+      p {
+        width: 100%;
+        text-align: justify;
+        line-height: 1.2;
+        margin-bottom: 1em;
+      }
     }
   }
 `;
@@ -39,8 +57,23 @@ export const ContentTall = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+
   img {
     width: 50%;
     margin-right: 2em;
+  }
+
+  @media (max-width: 425px) {
+    display: block;
+
+    img {
+      width: 100%;
+      margin-right: 0;
+    }
+
+    p {
+      text-align: justify;
+      line-height: 1.2;
+    }
   }
 `;
